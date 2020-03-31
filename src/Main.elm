18e50ec8d17be3d50516ace7b5de5e9d70f92a5e
nixCodeX <nixCodeX@users.noreply.github.com>
2020-03-31 17:17:22 +0100

    Add Import of Psalms

--- src/Main.elm
+++ src/Main.elm
@@ -3,26 +3,36 @@ module Main exposing (..)
 import Basics.Extra
 import Bootstrap.Button as Button
 import Bootstrap.CDN as CDN
+import Bootstrap.Form as Form
 import Bootstrap.Form.Input as Input
+import Bootstrap.Form.Radio as Radio
 import Bootstrap.Grid as Grid
 import Bootstrap.Grid.Col as Col
 import Bootstrap.Grid.Row as Row
+import Bootstrap.Modal as Modal
 import Bootstrap.Navbar as Navbar
 import Bootstrap.Table as Table
 import Browser
 import Browser.Events
+import Debug
 import Delay
 import File
 import File.Select
 import Html
 import Html.Attributes
 import Html.Events
+import Html.Parser
 import Http
 import Json.Decode
 import List.Extra as List
 import Maybe.Extra as Maybe
 import Random
 import Task
+
+
+logId : a -> a
+logId x =
+    Debug.log (Debug.toString x) x
 
 
 transpose : Int -> List a -> List a
@@ -51,8 +61,138 @@ removeAt n_ xs_ =
             x :: removeAt (n - 1) xs
 
 
+lookup : a -> List ( a, b ) -> Maybe b
+lookup x yzs_ =
+    case yzs_ of
+        [] ->
+            Nothing
+
+        ( y, z ) :: yzs ->
+            if x == y then
+                Just z
+
+            else
+                lookup x yzs
+
+
+takeDropUntil : (a -> Bool) -> List a -> ( List a, List a )
+takeDropUntil f xs =
+    case xs of
+        [] ->
+            ( [], [] )
+
+        y :: ys ->
+            if f y then
+                ( [], xs )
+
+            else
+                Tuple.mapFirst ((::) y) (takeDropUntil f ys)
+
+
+findByAttribute : Html.Parser.Attribute -> List Html.Parser.Node -> Maybe Html.Parser.Node
+findByAttribute attr =
+    Maybe.orList
+        << List.map
+            (\node ->
+                case node of
+                    Html.Parser.Element tag attrs children ->
+                        if List.member attr attrs then
+                            Just node
+
+                        else
+                            findByAttribute attr children
+
+                    _ ->
+                        Nothing
+            )
+
+
+type alias PsalmVerse =
+    ( List String, List String )
+
+
+type alias Psalm =
+    List PsalmVerse
+
+
+getFirstHalfOfPsalmVerse : List Html.Parser.Node -> ( List Html.Parser.Node, List Html.Parser.Node )
+getFirstHalfOfPsalmVerse =
+    takeDropUntil
+        (\node ->
+            case node of
+                Html.Parser.Element tag attrs children ->
+                    attrs |> lookup "class" |> Maybe.withDefault "" |> String.split " " |> List.member "indent1"
+
+                _ ->
+                    False
+        )
+
+
+getSecondHalfOfPsalmVerse : List Html.Parser.Node -> ( List Html.Parser.Node, List Html.Parser.Node )
+getSecondHalfOfPsalmVerse =
+    takeDropUntil
+        (\node ->
+            case node of
+                Html.Parser.Element tag attrs children ->
+                    attrs |> lookup "class" |> Maybe.withDefault "" |> String.split " " |> List.member "ve1"
+
+                _ ->
+                    False
+        )
+
+
+extractPsalmText : List Html.Parser.Node -> List String
+extractPsalmText =
+    List.map
+        (\node ->
+            case node of
+                Html.Parser.Element tag attrs children ->
+                    children
+                        |> List.map
+                            (\child ->
+                                case child of
+                                    Html.Parser.Text s ->
+                                        s
+
+                                    _ ->
+                                        ""
+                            )
+                        |> String.join "\n"
+
+                _ ->
+                    ""
+        )
+
+
+parsePsalmVerses : List Html.Parser.Node -> Psalm
+parsePsalmVerses nodes =
+    if List.isEmpty nodes then
+        []
+
+    else
+        let
+            ( firstHalf, afterFirstHalf ) =
+                getFirstHalfOfPsalmVerse nodes
+        in
+        let
+            ( secondHalf, afterSecondHalf ) =
+                getSecondHalfOfPsalmVerse afterFirstHalf
+        in
+        ( extractPsalmText firstHalf, extractPsalmText secondHalf ) :: parsePsalmVerses afterSecondHalf
+
+
+getPsalmFile : Psalm -> List String
+getPsalmFile =
+    List.map
+        (\( firstHalf, secondHalf ) ->
+            String.join " " firstHalf ++ " : " ++ String.join " " secondHalf
+        )
+
+
 type alias Model =
     { navbarState : Navbar.State
+    , importPsalmModalVisibility : Modal.Visibility
+    , importPsalm : Int
     , url : String
     , seq : Int
     , manualCaption : String
@@ -68,6 +208,8 @@ type Msg
     | Batch (List Msg)
     | RunCmd (Cmd Msg)
     | NavbarMsg Navbar.State
+    | ImportPsalmModalVisibility Modal.Visibility
+    | ChangeImportPsalm Int
     | ChangeURL String
     | ChangeSeq Int
     | SendCaption Int Int String
@@ -75,6 +217,8 @@ type Msg
     | OfferOpenFiles
     | PerformOpenFiles (List File.File)
     | CompleteOpenFiles (List ( String, String ))
+    | ImportPsalm
+    | PerformImportPsalm Int (List Html.Parser.Node)
     | SelectFile Int
     | MoveUpFile Int
     | MoveDownFile Int
@@ -99,6 +243,8 @@ init () =
             Navbar.initialState NavbarMsg
     in
     ( { navbarState = navbarState
+      , importPsalmModalVisibility = Modal.hidden
+      , importPsalm = 1
       , url = ""
       , seq = 0
       , manualCaption = ""
@@ -121,6 +267,11 @@ view model =
             |> Navbar.brand [] [ Html.text "Zoom Captions" ]
             |> Navbar.items
                 [ Navbar.itemLink [ Html.Events.onClick OfferOpenFiles ] [ Html.text "Open" ]
+                , Navbar.dropdown
+                    { id = "import"
+                    , toggle = Navbar.dropdownToggle [] [ Html.text "Import" ]
+                    , items = [ Navbar.dropdownItem [ Html.Events.onClick (ImportPsalmModalVisibility Modal.shown) ] [ Html.text "CW Psalm" ] ]
+                    }
                 ]
             |> Navbar.customItems
                 [ Navbar.formItem []
@@ -133,6 +284,27 @@ view model =
                     ]
                 ]
             |> Navbar.view model.navbarState
+        , Modal.config (ImportPsalmModalVisibility Modal.hidden)
+            |> Modal.withAnimation ImportPsalmModalVisibility
+            |> Modal.body []
+                [ Form.form []
+                    [ Form.group []
+                        [ Html.text "Psalm"
+                        , Input.number
+                            [ Input.value << String.fromInt <| model.importPsalm
+                            , Input.onInput (Maybe.unwrap NOP ChangeImportPsalm << String.toInt)
+                            ]
+                        ]
+                    ]
+                ]
+            |> Modal.footer []
+                [ Button.button
+                    [ Button.outlinePrimary
+                    , Button.onClick <| Batch [ ImportPsalmModalVisibility Modal.hiddenAnimated, ImportPsalm ]
+                    ]
+                    [ Html.text "Import Psalm" ]
+                ]
+            |> Modal.view model.importPsalmModalVisibility
         , Grid.container []
             [ Grid.row []
                 [ Grid.col [ Col.sm ] [ Html.text "Manual Caption:" ]
@@ -231,6 +403,12 @@ update msg model =
         NavbarMsg navbarState ->
             ( { model | navbarState = navbarState }, Cmd.none )
 
+        ImportPsalmModalVisibility importPsalmModalVisibility ->
+            ( { model | importPsalmModalVisibility = importPsalmModalVisibility }, Cmd.none )
+
+        ChangeImportPsalm importPsalm ->
+            ( { model | importPsalm = importPsalm }, Cmd.none )
+
         ChangeURL url ->
             ( { model | url = url }, Cmd.none )
 
@@ -315,6 +493,43 @@ update msg model =
             , Cmd.none
             )
 
+        ImportPsalm ->
+            ( model
+            , Http.get
+                { url = "https://cors-anywhere.herokuapp.com/https://www.churchofengland.org/prayer-and-worship/worship-texts-and-resources/common-worship/common-material/psalter/psalm-" ++ String.fromInt model.importPsalm
+                , expect =
+                    Http.expectString
+                        (Result.toMaybe
+                            >> Maybe.andThen (String.replace "<!DOCTYPE html>" "" >> Html.Parser.run >> Result.toMaybe)
+                            >> Maybe.unwrap (ManualCaption "Failed") (PerformImportPsalm model.importPsalm)
+                        )
+                }
+            )
+
+        PerformImportPsalm number html ->
+            let
+                file =
+                    case logId <| findByAttribute ( "class", "cw" ) html of
+                        Just (Html.Parser.Element _ _ children) ->
+                            children
+                                |> parsePsalmVerses
+                                |> getPsalmFile
+
+                        _ ->
+                            []
+            in
+            ( { model
+                | files =
+                    model.files
+                        ++ [ ( "Psalm " ++ String.fromInt number
+                             , file
+                             )
+                           ]
+                , manualCaption = "Importing"
+              }
+            , Cmd.none
+            )
+
         SelectFile n ->
             ( { model
                 | captions = Maybe.unwrap [] Tuple.second <| List.getAt n <| model.files
@@ -345,6 +560,7 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch
         [ Navbar.subscriptions model.navbarState NavbarMsg
+        , Modal.subscriptions model.importPsalmModalVisibility ImportPsalmModalVisibility
         , Browser.Events.onKeyDown
             (Json.Decode.field "key" Json.Decode.string
                 |> Json.Decode.andThen
